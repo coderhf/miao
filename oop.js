fb0f29b0e1f0b114a91e0c93cbd31b9a51135185
hufeng <hufeng@qq.com>
2024-07-31 11:32:04 +0800

    111

--- oop.js
+++ oop.js
@@ -1,5 +1,3 @@
-
-
 // 书本第六章作业
 // 6.14.1 向量类型
 function Vector(x, y) {
@@ -22,7 +20,7 @@ Vector.prototype.minus = function (v) {
 Object.defineProperty(Vector.prototype, 'length', {
   get: function () {
     return Math.sqrt(this.x * this.x + this.y * this.y)
-  }
+  },
 })
 // let v1 = new Vector(1, 2)
 // let v2 = new Vector(3, -4)
@@ -60,8 +58,12 @@ Complex.prototype.multiple = function (c) {
 }
 
 Complex.prototype.div = function (c) {
-  let real = (this.real * c.real + this.imag * c.imag) / (c.real * c.real + c.imag * c.imag)
-  let imag = (this.imag * c.real - this.real * c.imag) / (c.real * c.real + c.imag * c.imag)
+  let real =
+    (this.real * c.real + this.imag * c.imag) /
+    (c.real * c.real + c.imag * c.imag)
+  let imag =
+    (this.imag * c.real - this.real * c.imag) /
+    (c.real * c.real + c.imag * c.imag)
   return new Complex(real, imag)
 }
 
@@ -80,8 +82,6 @@ Complex.prototype.toString = function () {
 
 // console.log(c6.toString()) // 2+3i
 // console.log(c2.toString()) // 2+3i
-
-
 
 // 表示一个单向链表
 function LinkedList() {
@@ -112,11 +112,11 @@ LinkedList.prototype.append = function (val) {
   // 哨兵结点
   let dummy = {
     val: 0,
-    next: this._head
+    next: this._head,
   }
   let node = {
-    val : val,
-    next: null
+    val: val,
+    next: null,
   }
   this._length++
   let cur = dummy
@@ -154,7 +154,7 @@ LinkedList.prototype.pop = function () {
 LinkedList.prototype.prepend = function (val) {
   let node = {
     val: val,
-    next: this._head
+    next: this._head,
   }
   this._length++
   this._head = node
@@ -179,11 +179,59 @@ LinkedList.prototype.toArray = function () {
   }
   return result
 }
-LinkedList.prototype.toString = function() {
+LinkedList.prototype.toString = function () {
   return this.toArray().join('->')
 }
 Object.defineProperty(LinkedList.prototype, 'length', {
-  get: function() {
+  get: function () {
     return this._length
-  }
+  },
 })
+
+// 表示一个集合（集合中元素没有序，但不能重复）
+// 构造函数可选的可以传入集合中的初始值，但会被去重后存放
+function MySet(initalValues = []) {
+  this._elements = []
+  for (let item of initalValues) {
+    this.add(item)
+  }
+}
+// 向集合中添加元素
+MySet.prototype.add = function (val) {
+  if (!this._elements.includes(val)) {
+    this._elements.push(val)
+  }
+  return this
+}
+// 从集合中删除item元素
+MySet.prototype.delete = function (item) {
+  let idx = this._elements.indexOf(item)
+  if (idx > 0) {
+    this._elements.splice(idx, 1)
+    return true
+  }
+  return false
+}
+
+// 获取集合中的元素用 c.size，它是一个getter
+Object.defineProperty(MySet.prototype, 'size', {
+  get: function () {
+    return this._elements.length
+  },
+})
+
+// 清空集合中的所有元素
+MySet.prototype.clear = function () {
+  this._elements = []
+}
+
+// 判断集合中是否存在某元素
+MySet.prototype.has = function (item) {
+  return this._elements.includes(item)
+}
+// 遍历集合中的元素（顺序无所谓）
+MySet.prototype.forEach = function (func) {
+  for (let item of this._elements) {
+    func(item)
+  }
+}
