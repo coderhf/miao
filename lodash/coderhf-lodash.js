2eb410551aa97a4a0163ee9bd8b8a6a10ca49ff3
hufeng <hufeng@qq.com>
2024-08-13 12:20:19 +0800

    111

--- lodash/coderhf-lodash.js
+++ lodash/coderhf-lodash.js
@@ -303,6 +303,21 @@ var coderhf = (function () {
     return array[0]
   }
 
+  function indexOf(array, value, fromIndex = 0) {
+    if (fromIndex >= 0) {
+      for (let i = fromIndex; i < array.length; i++) {
+        if (array[i] === value) {
+          return i
+        }
+      }
+    } else {
+      for (let i = fromIndex + array.length; i >= 0; i--) {
+        if (array[i] === value) return i
+      }
+    }
+    return -1
+  }
+
   return {
     isEqual: isEqual,
     matches: matches,
@@ -324,5 +339,7 @@ var coderhf = (function () {
     fromPairs: fromPairs,
     toPairs: toPairs,
     head: head,
+    indexOf: indexOf,
+
   }
 })()
